d4aa96b9c630ae2207f836b33cd518504a15fe62
Laurent Greyling <l.greyling@niposoftware.com>
2017-07-05 15:27:07 +0200

    Copy and backup table storage

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Data.Services.Common;
+using System.Threading.Tasks;
+using backup_storage.Entity;
 using backup_storage.Shared;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace backup_storage
 {
@@ -11,15 +15,24 @@ namespace backup_storage
         {
             var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
             var destStorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DestStorageConnectionString"));
-
+            
+            //Create and populate stuff
             Console.WriteLine("Creating and populating some more dummy tables....");
             CreateTableStorage.CreateAndPopulateTable(storageAccount);
             Console.WriteLine("Finished Creating and populating some more dummy tables....");
 
             Console.WriteLine("Creating and populating some more dummy blobs....");
             CreateBlobStorage.CreateAndPopulateBlob(storageAccount);
-            Console.WriteLine("Creating and populating some more dummy blobs....");
+            Console.WriteLine("Finished Creating and populating some more dummy blobs....");
 
+            //Copy and backup table storage
+            Console.WriteLine($"{Environment.NewLine}TABLE STORAGE");
+            Console.WriteLine("Start copying table storage to new destination storage");
+            BackupTableStorage.CopyTableStorage(storageAccount, destStorageAccount);
+            Console.WriteLine("Finished copying table storage to new destination storage");
+
+            Console.WriteLine($"{Environment.NewLine}BLOB STORAGE");
+            //Copy and backup blob
             Console.WriteLine("Start copying blob to new destination storage");
             BackupBlobStorage.ContainerCopyAndBackUp(storageAccount, destStorageAccount);
             Console.WriteLine("Finished copying blob to new destination storage");
---
+++ Shared/BackupTableStorage.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace backup_storage.Shared
+{
+    public class BackupTableStorage
+    {
+        public static void CopyTableStorage(CloudStorageAccount storageAccount, CloudStorageAccount destStorageAccount)
+        {
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var tables = tableClient.ListTables();
+
+            Parallel.ForEach(tables, async table =>
+            {
+                var query = new TableQuery();
+                var tblData = table.ExecuteQuery(query);
+
+                var tableClientDest = destStorageAccount.CreateCloudTableClient();
+                var tbl = tableClientDest.GetTableReference(table.Name);
+                tbl.CreateIfNotExists();
+
+                foreach (var dataEntity in tblData)
+                {
+                    var insertData = TableOperation.Insert(dataEntity);
+                    await tbl.ExecuteAsync(insertData);
+                }
+            });
+        }
+    }
+}
