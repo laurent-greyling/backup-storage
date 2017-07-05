1d7f69327f5b8ca0f097c9b229c0f9868085a340
Laurent Greyling <l.greyling@niposoftware.com>
2017-07-05 14:21:47 +0200

    Copy src blob to dest blob for backup purposes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using backup_storage.Entity;
 using backup_storage.Shared;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
-using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace backup_storage
 {
@@ -17,10 +10,21 @@ namespace backup_storage
         public static void Main(string[] args)
         {
             var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            var destStorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DestStorageConnectionString"));
 
+            Console.WriteLine("Creating and populating some more dummy tables....");
             CreateTableStorage.CreateAndPopulateTable(storageAccount);
+            Console.WriteLine("Finished Creating and populating some more dummy tables....");
 
+            Console.WriteLine("Creating and populating some more dummy blobs....");
             CreateBlobStorage.CreateAndPopulateBlob(storageAccount);
+            Console.WriteLine("Creating and populating some more dummy blobs....");
+
+            Console.WriteLine("Start copying blob to new destination storage");
+            BackupBlobStorage.ContainerCopyAndBackUp(storageAccount, destStorageAccount);
+            Console.WriteLine("Finished copying blob to new destination storage");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Shared/BackupBlobStorage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace backup_storage.Shared
+{
+    public class BackupBlobStorage
+    {
+        public static void ContainerCopyAndBackUp(CloudStorageAccount storageAccount, CloudStorageAccount destStorageAccount)
+        {
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var containers = blobClient.ListContainers();
+
+            Parallel.ForEach(containers, container =>
+            {
+                var blobClientDest = destStorageAccount.CreateCloudBlobClient();
+                var ctn = blobClientDest.GetContainerReference(container.Name);
+                
+                ctn.CreateIfNotExists();
+
+                CopyBlobsAndBackup(container, ctn);
+            });
+        }
+
+        public static void CopyBlobsAndBackup(
+            CloudBlobContainer srcContainer,
+            CloudBlobContainer destContainer)
+        {
+
+            var policyId = new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(24),
+                Permissions = SharedAccessBlobPermissions.Read
+            };
+
+            // get the SAS token to use for all blobs
+            var sas = srcContainer.GetSharedAccessSignature(policyId);
+            
+            var srcBlobList = srcContainer.ListBlobs();
+
+            Parallel.ForEach(srcBlobList, async src =>
+            {
+                var srcBlob = (CloudBlob) src;
+
+                // Create appropriate destination blob type to match the source blob
+                CloudBlob destBlob;
+                if (srcBlob.GetType() == typeof(CloudBlockBlob))
+                {
+                    srcBlob = (CloudBlockBlob) src;
+                    destBlob = destContainer.GetBlockBlobReference(srcBlob.Name);
+                }
+                else
+                {
+                    destBlob = destContainer.GetPageBlobReference(srcBlob.Name);
+                }
+
+                var srcBlockBlobSasUri = $"{srcBlob.Uri}{sas}";
+                // copy using src blob as SAS
+                await destBlob.StartCopyAsync(new Uri(srcBlockBlobSasUri));
+            });
+        }
+    }
+}
