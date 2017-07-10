c4b628422609a7358beff78296b06f62dcfed05d
Laurent Greyling <l.greyling@niposoftware.com>
2017-07-10 10:06:22 +0200

    Added options for the command line arguments in order to be more specific in what want to be done

---
+++ Options.cs
@@ -0,0 +1,43 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace backup_storage
+{
+    internal class Options
+    {
+        [Option('b', "backup", HelpText = "backups up table and blob storage", Required = false, DefaultValue = true)]
+        public bool Backup { get; set; }
+
+        [Option('r', "restore", HelpText = "restore table and blob storage", Required = false, DefaultValue = false)]
+        public bool Restore { get; set; }
+
+        [Option('f', "fillstorage", HelpText = "indicate if storage should be filled with dummy info", Required = false, DefaultValue = false)]
+        public bool FillStorage { get; set; }
+
+        [Option('t', "tables", HelpText = "list of tables to restore", Required = false)]
+        public string Tables { get; set; }
+
+        [Option('c', "containers", HelpText = "list of containers to restore", Required = false)]
+        public string Containers { get; set; }
+
+        [Option('s', "storageconnectionstring", HelpText = "connectionstring to storage that need to be backedup or restored", Required = true)]
+        public string StorageConnectionString { get; set; }
+
+        [Option('d', "deststorageconnectionstring", HelpText = "destination connectionstring of storage where storage need to be backedup or restored to", Required = true)]
+        public string DestStorageConnectionString { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("backup-storage.exe", "1.00.000"),
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true,
+            };
+            help.AddPreOptionsLine("Usage: [options]");
+            help.AddOptions(this);
+            return help;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using backup_storage.Shared;
-using Microsoft.Azure;
+using CommandLine;
 using Microsoft.WindowsAzure.Storage;
 
 namespace backup_storage
@@ -10,15 +10,21 @@ namespace backup_storage
     {
         public static void Main(string[] args)
         {
-            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
-            var destStorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DestStorageConnectionString"));
+            var options = new Options();
 
+            if (!Parser.Default.ParseArguments(args, options)) return;
+            
+            var storageAccount = CloudStorageAccount.Parse(options.StorageConnectionString); //CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            var destStorageAccount = CloudStorageAccount.Parse(options.DestStorageConnectionString); //CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DestStorageConnectionString"));
+            
             var swTable = new Stopwatch();
             var swblob = new Stopwatch();
 
-            //Run this to n to create n tables and blobs, comment the for loop in if want to create bunch op info for copy
-            //for (var i = 0; i < 10; i++)
-            //{
+            if (options.FillStorage)
+            {
+                //Run this to n to create n tables and blobs, comment the for loop in if want to create bunch op info for copy
+                //for (var i = 0; i < 10; i++)
+                //{
                 //Create and populate stuff
                 Console.WriteLine("Creating and populating some more dummy tables....");
                 CreateTableStorage.CreateAndPopulateTable(storageAccount);
@@ -27,26 +33,34 @@ namespace backup_storage
                 Console.WriteLine("Creating and populating some more dummy blobs....");
                 CreateBlobStorage.CreateAndPopulateBlob(storageAccount);
                 Console.WriteLine("Finished Creating and populating some more dummy blobs....");
-            //}
+                //}
+            }
 
-            //Copy and backup table storage
-            Console.WriteLine($"{Environment.NewLine}TABLE STORAGE");
-            Console.WriteLine("Start copying table storage to new destination storage");
-            swTable.Start();
-            //BackupTableStorage.CopyTableStorage(storageAccount, destStorageAccount); //This is only in paralell
-            BackupTableStorage.CopyAndBackUpTableStorage(storageAccount, destStorageAccount).Wait(); //This is with dataflow - this is the quicker copy method
-            Console.WriteLine($"Finished copying table storage to new destination storage - {swTable.Elapsed}");
-            swTable.Stop();
+            if (options.Backup)
+            {
+                //Copy and backup table storage
+                Console.WriteLine($"{Environment.NewLine}TABLE STORAGE");
+                Console.WriteLine("Start copying table storage to new destination storage");
+                swTable.Start();
+                //BackupTableStorage.CopyTableStorage(storageAccount, destStorageAccount); //This is only in paralell
+                BackupTableStorage.CopyAndBackUpTableStorage(storageAccount, destStorageAccount).Wait(); //This is with dataflow - this is the quicker copy method
+                Console.WriteLine($"Finished copying table storage to new destination storage - {swTable.Elapsed}");
+                swTable.Stop();
 
-            Console.WriteLine($"{Environment.NewLine}BLOB STORAGE");
-            //Copy and backup blob
-            Console.WriteLine("Start copying blob to new destination storage");
-            swblob.Start();
-            //BackupBlobStorage.CopyBlobStorage(storageAccount, destStorageAccount); //This is only in paralell
-            BackupBlobStorage.BackupBlobToStorage(storageAccount, destStorageAccount).Wait(); //This is only in DataFlow - somewhat faster, but currently by not much
-            Console.WriteLine($"Finished copying blob to new destination storage - {swblob.Elapsed}");
-            swblob.Stop();
+                Console.WriteLine($"{Environment.NewLine}BLOB STORAGE");
+                //Copy and backup blob
+                Console.WriteLine("Start copying blob to new destination storage");
+                swblob.Start();
+                //BackupBlobStorage.CopyBlobStorage(storageAccount, destStorageAccount); //This is only in paralell
+                BackupBlobStorage.BackupBlobToStorage(storageAccount, destStorageAccount).Wait(); //This is only in DataFlow - somewhat faster, but currently by not much
+                Console.WriteLine($"Finished copying blob to new destination storage - {swblob.Elapsed}");
+                swblob.Stop();
+            }
 
+            if (options.Restore)
+            {
+                //do stuff
+            }
             Console.ReadKey();
         }
     }
