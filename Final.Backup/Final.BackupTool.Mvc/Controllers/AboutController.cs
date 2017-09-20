ae3f98fcb5da6697bcc49224788d33a747d75f54
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2017-09-20 19:06:53 +0200

    Added about view

---
+++ Final.Backup/Final.BackupTool.Mvc/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Final.BackupTool.Mvc.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
